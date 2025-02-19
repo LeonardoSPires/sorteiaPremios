7ac4f52357e42ff288a9dbc9c2e9bf0bd7a91247
Leonardo Pires <leopires1988@gmail.com>
2025-02-19 20:10:54 -0300

    projeto finalizado!

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
+
+
 function generateNumber(min, max) {
-    const numeroSorteado = document.querySelector('.numero-sorteado');
+    
     const premioSorteado = document.querySelector('.premio-sorteado');
     const btnSorteio = document.querySelector('.btn-sorteio');
-    const premioSorteio = document.querySelector('.premio');
+    const numeroSorteado = document.querySelector('.numero-sorteado');
 
     premioSorteado.style.display ="block";
     btnSorteio.style.display ="none";
@@ -12,27 +14,41 @@ function generateNumber(min, max) {
 
     const result = Math.floor(Math.random() * (max - min +1)) + min;
 
+    if (result === 1) {
+      premioSorteado.innerText ="Parabéns você acaba de ganhar um(a): Celular"
+    }
+    else if (result === 2) {
+      premioSorteado.innerText ="Parabéns você acaba de ganhar um(a): Video Game"
+    }
+    else if (result === 3) {
+      premioSorteado.innerText ="Parabéns você acaba de ganhar um(a): Tv 65"
+    }
+    else if (result === 4) {
+        premioSorteado.innerText ="Parabéns você acaba de ganhar um(a): Moto"
+    }
+    else if (result === 5) {
+      premioSorteado.innerText ="Parabéns você acaba de ganhar um(a): Carro"
+    }
+    else if (result === 6) {
+      premioSorteado.innerText ="Parabéns você acaba de ganhar um(a): Casa"
+    }else{
+        premioSorteado.innerText ="Tente novamente!"
+    }
+
     numeroSorteado.innerText = result;
 
-    if (result == "1") {
-        premioSorteio.innerText ="Celular"
-    }
-    else if (result == "2") {
-        premioSorteio.innerText ="Video Game"
-    }
-    else if (result == "3") {
-        premioSorteio.innerText ="Tv 65"
-    }
-    else if (result == "4") {
-        premioSorteio.innerText ="Moto"
-    }
-    else if (result == "5") {
-        premioSorteio.innerText ="Carro"
-    }
-    else if (result == "6") {
-        premioSorteio.innerText ="Casa"
-    }else{
-        premioSorteado .innerText ="Tente novamente!"
-    }
-
 }
+
+function credito() {
+    const btnVoltar = document.getElementById('btn-voltar');
+    const premioApagar = document.getElementById('premio-apagar');
+
+    btnVoltar.style.display ="block";
+    premioApagar.style.display ="none";
+}
+
+
+
+
+
+
